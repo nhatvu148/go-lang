df628792287dcf3bb08afbcf0dcb6ccd32e80eb3
unknown <nhatvu148@gmail.com>
2020-10-22 16:56:18 +0900

    channel loop

--- channel/main.go
+++ channel/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,19 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+	// for  {
+	// 	go checkLink(<-c, c)
+	// }
+
+	for l := range c {
+		// go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 
 }
@@ -30,10 +42,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yeah it's up"
+	c <- link
 }
