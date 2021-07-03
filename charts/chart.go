1c430da38a374a930108d1556ea1f334242c12f4
nhatvu148 <nhatvu148@gmail.com>
2021-07-03 15:01:23 +0900

    adding canvas

--- charts/chart.go
+++ charts/chart.go
@@ -12,7 +12,6 @@ import (
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
-	"gonum.org/v1/plot/vg/draw"
 )
 
 func main() {
@@ -28,27 +27,28 @@ func main() {
 
 	// Get some random points
 	rand.Seed(int64(0))
-	n := 15
-	scatterData := randomPoints(n)
+	n := 50
+	// scatterData := randomPoints(n)
 	lineData := randomPoints(n)
-	linePointsData := randomPoints(n)
+	fmt.Printf("%T, %v\n", lineData, lineData)
+	// linePointsData := randomPoints(n)
 
 	// Create a new plot, set its title and
 	// axis labels.
 	p := plot.New()
 
-	p.Title.Text = "Points Example"
-	p.X.Label.Text = "X"
-	p.Y.Label.Text = "Y"
+	p.Title.Text = "DFP"
+	// p.X.Label.Text = "X"
+	p.Y.Label.Text = "Stress [MPa]"
 	// Draw a grid behind the data
 	p.Add(plotter.NewGrid())
 
 	// Make a scatter plotter and set its style.
-	s, err := plotter.NewScatter(scatterData)
-	if err != nil {
-		panic(err)
-	}
-	s.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
+	// s, err := plotter.NewScatter(scatterData)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// s.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
 
 	// Make a line plotter and set its style.
 	l, err := plotter.NewLine(lineData)
@@ -56,24 +56,25 @@ func main() {
 		panic(err)
 	}
 	l.LineStyle.Width = vg.Points(1)
-	l.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+	// l.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
 	l.LineStyle.Color = color.RGBA{B: 255, A: 255}
 
 	// Make a line plotter with points and set its style.
-	lpLine, lpPoints, err := plotter.NewLinePoints(linePointsData)
-	if err != nil {
-		panic(err)
-	}
-	lpLine.Color = color.RGBA{G: 255, A: 255}
-	lpPoints.Shape = draw.PyramidGlyph{}
-	lpPoints.Color = color.RGBA{R: 255, A: 255}
+	// lpLine, lpPoints, err := plotter.NewLinePoints(linePointsData)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// lpLine.Color = color.RGBA{G: 255, A: 255}
+	// lpPoints.Shape = draw.PyramidGlyph{}
+	// lpPoints.Color = color.RGBA{R: 255, A: 255}
 
 	// Add the plotters to the plot, with a legend
 	// entry for each
-	p.Add(s, l, lpLine, lpPoints)
-	p.Legend.Add("scatter", s)
+	p.Add(l)
+	// p.Add(s, l, lpLine, lpPoints)
+	// p.Legend.Add("scatter", s)
 	p.Legend.Add("line", l)
-	p.Legend.Add("line points", lpLine, lpPoints)
+	// p.Legend.Add("line points", lpLine, lpPoints)
 
 	start := time.Now()
 
--- charts/go.mod
+++ charts/go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/go-echarts/go-echarts/v2 v2.2.4
-	github.com/wcharczuk/go-chart/v2 v2.1.0
+	github.com/paulmach/orb v0.2.2
+	github.com/paulmach/osm v0.2.2
+	github.com/tdewolff/canvas v0.0.0-20210629004453-6d68f6ba1416
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
-	gonum.org/v1/plot v0.9.0 // indirect
+	gonum.org/v1/plot v0.9.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
