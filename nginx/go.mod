56f944ef4bd2544dd6544560b5488fe5d51c9cf9
nhatvu148 <nhatvu148@gmail.com>
2021-07-05 00:30:49 +0900

    kill nginx 2 process

--- nginx/go.mod
+++ nginx/go.mod
@@ -1,3 +1,8 @@
 module github.com/nhatvu148/go-lang/nginx
 
 go 1.16
+
+require (
+	github.com/shirou/gopsutil/v3 v3.21.6 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
+)
--- nginx/main.go
+++ nginx/main.go
@@ -6,32 +6,68 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"sync"
+	"syscall"
 	"time"
+	"unsafe"
+
+	"github.com/shirou/gopsutil/v3/process"
+	"golang.org/x/sys/windows"
 )
+
+const TH32CS_SNAPPROCESS = 0x00000002
+
+// WindowsProcess is an implementation of Process for Windows.
+type WindowsProcess struct {
+	ProcessID       int
+	ParentProcessID int
+	Exe             string
+}
 
 func main() {
 	start := time.Now()
-	port := flag.Int("port", 52525, "Port number")
+	port := flag.Int("port", 12345, "Port number")
 	nginxDir := flag.String("nginxDir", "C:/Users/nhatv/Work/TechnoStar/jmu-dt/bin/nginx", "Nginx directory")
 
 	flag.Parse()
 
 	var wg sync.WaitGroup
-	wg.Add(1)
+	sig := make(chan struct{})
+	wg.Add(2)
 
-	// go editNginx(*port, fmt.Sprintf("%s/conf/nginx.conf", *nginxDir), &wg)
-	go func(port int, nginxDir string, wg *sync.WaitGroup) {
-		editNginx(port, fmt.Sprintf("%s/conf/nginx.conf", nginxDir), wg)
-	}(*port, *nginxDir, &wg)
+	go editNginx(*port, fmt.Sprintf("%s/conf/nginx.conf", *nginxDir), &wg, sig)
+	go restartNginx(*nginxDir, &wg, sig)
 
 	wg.Wait()
+
 	elapsed := time.Since(start)
 	log.Printf("Editing config took %s", elapsed)
 }
 
-func editNginx(port int, confFile string, wg *sync.WaitGroup) {
+func restartNginx(nginxDir string, wg *sync.WaitGroup, sig chan struct{}) {
 	defer wg.Done()
+	<-sig
+
+	findKillProcess()
+	findKillProcess()
+}
+
+func findKillProcess() {
+	procs, err := processes()
+	if err != nil {
+		log.Fatal(err)
+	}
+	nginx_dt := findProcessByName(procs, "nginx_dt.exe")
+	if nginx_dt != nil {
+		// found it
+		KillProcess(nginx_dt.ProcessID)
+	}
+}
+
+func editNginx(port int, confFile string, wg *sync.WaitGroup, sig chan struct{}) {
+	defer wg.Done()
+	defer close(sig)
 	file, err := os.Create(confFile)
 	if err != nil {
 		log.Fatal(err)
@@ -56,13 +92,83 @@ func editNginx(port int, confFile string, wg *sync.WaitGroup) {
 	}
 
 	for _, line := range lines {
-		bytesWritten, err := writer.WriteString(line + "\n")
+		_, err := writer.WriteString(line + "\n")
 		if err != nil {
 			log.Fatalf("Got error while writing to a file. Err: %s", err.Error())
 		}
-		fmt.Printf("Bytes Written: %d\n", bytesWritten)
-		fmt.Printf("Available: %d\n", writer.Available())
-		fmt.Printf("Buffered : %d\n", writer.Buffered())
+		// fmt.Printf("Bytes Written: %d\n", bytesWritten)
+		// fmt.Printf("Available: %d\n", writer.Available())
+		// fmt.Printf("Buffered : %d\n", writer.Buffered())
 	}
 	writer.Flush()
+}
+
+func processes() ([]WindowsProcess, error) {
+	handle, err := windows.CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer windows.CloseHandle(handle)
+
+	var entry windows.ProcessEntry32
+	entry.Size = uint32(unsafe.Sizeof(entry))
+	// get the first process
+	err = windows.Process32First(handle, &entry)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make([]WindowsProcess, 0, 50)
+	for {
+		results = append(results, newWindowsProcess(&entry))
+
+		err = windows.Process32Next(handle, &entry)
+		if err != nil {
+			// windows sends ERROR_NO_MORE_FILES on last process
+			if err == syscall.ERROR_NO_MORE_FILES {
+				return results, nil
+			}
+			return nil, err
+		}
+	}
+}
+
+func findProcessByName(processes []WindowsProcess, name string) *WindowsProcess {
+	for _, p := range processes {
+		if strings.ToLower(p.Exe) == strings.ToLower(name) {
+			return &p
+		}
+	}
+	return nil
+}
+
+func newWindowsProcess(e *windows.ProcessEntry32) WindowsProcess {
+	// Find when the string ends for decoding
+	end := 0
+	for {
+		if e.ExeFile[end] == 0 {
+			break
+		}
+		end++
+	}
+
+	return WindowsProcess{
+		ProcessID:       int(e.ProcessID),
+		ParentProcessID: int(e.ParentProcessID),
+		Exe:             syscall.UTF16ToString(e.ExeFile[:end]),
+	}
+}
+
+func KillProcess(pid int) {
+	processes, err := process.Processes()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, p := range processes {
+		if p.Pid == int32(pid) {
+			fmt.Println(p.Pid)
+			p.Kill()
+			// p.Terminate()
+		}
+	}
 }
